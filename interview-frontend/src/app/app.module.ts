9c21a15671735631ecda74bf05a4121192d920ec
GigiDev7 <gigivadachkoria@gmail.com>
2023-07-28 21:55:11 +0400

    added basic styling

--- interview-frontend/src/app/app.module.ts
+++ interview-frontend/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { CityCardComponent } from './city-card/city-card.component';
@@ -13,6 +14,7 @@ import { CityCardComponent } from './city-card/city-card.component';
     AppRoutingModule,
     HttpClientModule,
     MatPaginatorModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
